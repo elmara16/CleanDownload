95e0fe0d6e107a89f74a260ff5cd62644936668c
fjola17 <34029285+fjola17@users.noreply.github.com>
2018-12-11 09:14:46 +0000

    improved version of files

--- clean.py
+++ clean.py
@@ -1,24 +1,41 @@
 ##################
-from os import *
+from os import walk
 from pathlib import Path
 import re
+
+season = re.compile('(S|s)[0-9][0-9]')
+season_v2 = re.compile('Season [0-9][0-9]')
+season_v3 = re.compile('[0-9]x[0-9][0-9]')
+
+#find information about season /episode from a single file
+def work_with_file(filename):
+    ble = filename.replace('_', ' ').replace(".", " ")
+    
+
+#work with directories, check if they are already sorted or not
+def work_with_dir(dirname):
+    bla = dirname.replace('.', ' ')
+    search_first_pattern = re.search('(S|s)[0-9][0-9]', dirname)
+    if search_first_pattern:
+        blee = search_first_pattern.group()
+        print(blee)
 
 def search(foldername):
     #using Path
     filepath = Path(foldername)
-    some = set()
-    for i in filepath.glob('downloads/**/*.*'):
-        some.add(i)        
-    return some
-
+    some = list()
     # If using OS
-    # counter = 0
+    counter = 0
 
-    #for x, y, z in walk(filepath):
-    #    print(x, y, z)
-    #    counter += 1
-    #    if counter == 2:
-    #        break
+    for x in walk(filepath):
+        some.append([x,y,z])
+        #if counter == 1:
+        #    break
+        #counter += 1
+        #if isdir(): #checks if directory
+        #    work_with_dir(x)
+        #elif z.isfile():
+        #   pass #do stuff
         
 
 #Búa til fall til þess að lesa metadata út úr hverjum file, nota regex til að gera það
